d269d058825853c494a80d1df9fc44c3d329a3d0
AmosTurner <turneamo@sheridan.desire2learn.com>
2023-04-09 19:09:46 -0400

    Redid inventory system
    Created inventory from scratch again.

--- Assets/Scripts/InventoryScripts/ClickEvent.cs
+++ Assets/Scripts/InventoryScripts/ClickEvent.cs
@@ -2,11 +2,24 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ClickEvent : MonoBehaviour, IPointerClickHandler
+public class ClickEvent : MonoBehaviour
 {
     public GameObject keyImage;
-    public void OnPointerClick(PointerEventData eventData)
+    public GameObject keyPrefab;
+    public GameObject inventorySlot;
+    public void OnMouseDown()
     {
-        Debug.Log("Puzzle item clicked!");
+        Debug.Log("Puzzle found!");
+
+        // Instantiate the prefab as a child of the canvas
+        GameObject newObject = Instantiate(keyImage, inventorySlot.transform);
+        newObject.transform.localPosition = Vector3.zero;
+        keyPrefab.SetActive(false);
     }
+
+    public void UsePuzzle()
+    {
+        Debug.Log("Puzzle used!");
+    }
+
 }
